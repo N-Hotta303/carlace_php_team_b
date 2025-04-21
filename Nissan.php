2914b935aa0bb6a86244482f05441c4fcbc8c5e7
Ikeno Taiki <taiki.ikeno@tokyo.supersoftware.co.jp>
2025-04-21 16:42:09 +0900

    Honda.phpを追加

--- Nissan.php
+++ Nissan.php
@@ -1,8 +1,9 @@
 <?php
-require_once("Car.php");
+require_once("car.php");
 
 class Nissan extends Car{
     public function __construct(){
-        parent::__construct('Nissan', 3, 4, 200, 300, 6, 200);
+        parent::__construct($brand='Nissan',$capMin=3, $capMax=4, $priceMin=200, $priceMax=300, $accel=6, $maxSpeed=200);
     }
 }
+?>
