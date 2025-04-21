0725ba06b9671f7b34a7bf1390ed158a2db379bc
Ikeno Taiki <taiki.ikeno@tokyo.supersoftware.co.jp>
2025-04-21 16:17:46 +0900

    ホンダ、日産、フェラーリの子クラス３つ作りました

---
+++ Ferrari.php
@@ -0,0 +1,9 @@
+<?php
+require_once("car.php");
+
+class Ferrari extends Car{
+    public function __construct(){
+        parent::__construct($brand='Ferrari',$capMin=1, $capMax=2, $priceMin=2000, $priceMax=4000, $accel=12, $maxSpeed=200);
+    }
+}
+?>
