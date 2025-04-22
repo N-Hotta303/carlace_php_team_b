e62033721f9be388edbc5b66ab348cdaf26d87bd
Rei_Hagiwara <rei.hagiwara@tokyo.supersoftware.co.jp>
2025-04-22 11:35:28 +0900

    Q4回答入れます

---
+++ Question4/Nissan.php
@@ -0,0 +1,13 @@
+<?php
+require_once("car.php");
+session_start(); //欠陥による加速度低下を反映させるためここで開始
+
+class Nissan extends Car{
+    public function __construct(){
+        parent::__construct($brand='Nissan',$capMin=3, $capMax=4, $priceMin=200, $priceMax=300, $accel=6, $maxSpeed=200);
+
+        $this->acceleration = round($this->acceleration * 0.6, 2); //欠陥により60%の加速度に
+        $_SESSION["cars"]["Nissan"]["acceleration"] = $this->acceleration;
+    }
+}
+?>
