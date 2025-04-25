1eeb532740db4de5ab57c179451dee8e87d60773
hotta <naohiro.hotta@tokyo.supersoftware.co.jp>
2025-04-25 10:44:06 +0900

    accelの代入が余剰だったので削除

--- Cars/Nissan.php
+++ Cars/Nissan.php
@@ -7,8 +7,6 @@ class Nissan extends Car{
         parent::__construct($brand='Nissan',$capMin=3, $capMax=4, $priceMin=200, $priceMax=300, $accel=6, $maxSpeed=55, $brake_capa=0.5);
 
         $this->acceleration = round($this->acceleration * 0.6, 2); //欠陥により60%の加速度に
-        $_SESSION["accel"][$index] = $this->acceleration;
-
  
         $this->liftCount = $this->getLift_up();
         $this->adjustAcceleration($this->liftCount);
